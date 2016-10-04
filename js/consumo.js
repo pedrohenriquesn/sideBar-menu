d1d8fb57c07845dd92591ed19833299565ff304c
pedrohsn <pedrohsn.developer@gmail.com>
2016-10-04 12:49:14 -0300

    desenvolvimento do leyout da pagina de consumos

--- js/consumo.js
+++ js/consumo.js
@@ -1,9 +1,13 @@
+
 $(document).ready(function() {
 
-    $( document.body ).on( "click", ".box-consumo", function(){
-        image = $( this ).find( '.img-opcao' )
-        $(image).toggleClass('box-selected', 1000, "easeOutSine");
+    $('[data-toggle="tooltip"]').tooltip();
+
+    $( document.body ).on( "click", ".box-option", function(){
+        image = $(this).find('.icon-option')
+        text = $(this).find('.text-option')
+        $(this).toggleClass('box-selected', 300, "easeOutSine");
+        $(image).toggleClass('icon-selected', 500, "easeOutSine");
+        $(text).toggleClass('text-selected', 500, "easeOutSine");
     });
-
-
 });
